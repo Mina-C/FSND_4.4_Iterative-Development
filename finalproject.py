d3988679f8be7b4ac635766f5716f3501e5c7390
Mina C <mina.choe@outlook.com>
2019-08-02 23:54:02 +0900

    feat: link database with showMenu()

--- finalproject.py
+++ finalproject.py
@@ -62,6 +62,11 @@ def deleteRestaurant(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/')
 @app.route('/restaurants/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
     courseType=['Appetizer', 'Entree', 'Beverage', 'Dessert']
     return render_template('menu.html', restaurant=restaurant, items=items, courseType=courseType)
 
