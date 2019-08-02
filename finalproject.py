962be1943b540aad8c79742a331e2587bd26827c
Mina C <mina.choe@outlook.com>
2019-08-02 23:12:21 +0900

    feat: add Flask modules for CRUD, add create restaurant function

--- finalproject.py
+++ finalproject.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
-
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
+from flask import Flask, render_template, url_for, request, redirect
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
 
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    restaurants = session.query(Restaurant).all()
     return render_template('restaurants.html', restaurants=restaurants)
 
-@app.route('/restaurants/new/')
+@app.route('/restaurants/new/', methods=['GET', 'POST'])
 def newRestaurant():
-    return render_template('newRestaurants.html')
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    if request.method == 'POST':
+        newRestaurantName = Restaurant(name=request.form['name'])
+        session.add(newRestaurantName)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
+    else:
+        return render_template('newRestaurants.html')
 
 @app.route('/restaurants/<int:restaurant_id>/edit/')
 def editRestaurant(restaurant_id):
